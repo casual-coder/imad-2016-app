7afebc91c4d8439dafa11186a960d1a1b213cac7
casual-coder <rachna.1997@gmail.com>
2016-09-27 21:39:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 
+//change the text
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//Move the image
+var img = getElementById('madi');
+madi.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
