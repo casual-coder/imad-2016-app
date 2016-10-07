f8c9536c000b9e7067621ba70e45ac306ddba5dd
casual-coder <rachna.1997@gmail.com>
2016-10-07 20:28:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
   request.open('GET', 'http://casual-coder.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onlick = function () {
+  //Make a request to a server and send the name
+  
+  //Capture the list of names and render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for(var i=0; i<names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
