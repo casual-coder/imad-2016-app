415d8890664d98ac8ae9c8feee75470f71dcf758
casual-coder <rachna.1997@gmail.com>
2016-11-12 17:24:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 var config = {
     user : 'casual-coder',
     database : 'casual-coder',
@@ -12,6 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles = { 
     'article-one' : {
@@ -158,6 +161,21 @@ function hash(input, salt) {
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'some-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res) {
+   //username, password
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('insert into "user"(username, password) values($1, $2)',[username, dbString], function(err, result){
+        if(err){
+            res.status(500).send(err.toString()) ;
+        }else{
+            res.send('USER successfully create : ' + username);
+        }
+   });
 });
 
 
