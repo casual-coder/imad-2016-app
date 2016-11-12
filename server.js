6c5ddddf275467c7ba510d1479f7a41421f16a16
casual-coder <rachna.1997@gmail.com>
2016-11-12 16:51:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/articles/:articleName', function(req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 512, 10000, 'sha512');  
-    return hashed.toString('hex');
+    return ['pbkdf2', "10000", salt, hashed.toString('hex')].join('$');
 } 
 
 
