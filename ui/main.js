3f9b3df718d6d8e4ddbb3354fbe5832709d3e7a2
casual-coder <rachna.1997@gmail.com>
2016-11-12 19:49:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,6 @@
-//Counter code
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-  
-  //Create a request object
-  var request = new XMLHttpRequest();
-  
-  //Capture the response and stor it in a variable
-  request.onreadystatechange = function () {
-      if(request.readyState === XMLHttpRequest.DONE ) {
-          //Take some action
-          if(request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-      //No action
-  };
-  
-  //Make the request
-  request.open('GET', 'http://casual-coder.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
 
-//Submit name
+//Submit username, password
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //Create a request object
@@ -35,23 +11,23 @@ submit.onclick = function () {
       if(request.readyState === XMLHttpRequest.DONE ) {
           //Take some action
           if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0; i<names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                 }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-          }
+             console.log('User logged in');
+             alert('Logged in successfully');
+      }else if(request.status === 403) {
+          alert("Invalid username/password");
+      }else if(request.status === 500) {
+          alert("Something went wrong!");
       }
-      //No action
+    }  //No action
   };
   
   //Make the request
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET', 'http://casual-coder.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', 'http://casual-coder.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username:username, password:password}));
 
 };
