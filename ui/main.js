d76709c7723e4f3cce467575c7a1759983d31b38
casual-coder <rachna.1997@gmail.com>
2016-11-13 12:10:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,13 +13,19 @@ submit.onclick = function () {
           if(request.status === 200){
              console.log('User logged in');
              alert('Logged in successfully');
-      }else if(request.status === 403) {
+      } else if(request.status === 403) {
           alert('Invalid username/password');
-      }else if(request.status === 500) {
+      } else if(request.status === 500) {
           alert('Something went wrong!');
+          submit.value = 'Login';
+      } else{
+          alert('Something went wrong!');
+          submit.value = 'Login';
       }
-    }  //No action
+    }  
   };
+  
+  
   
   //Make the request
   var username = document.getElementById('username').value;
@@ -29,5 +35,36 @@ submit.onclick = function () {
   request.open('POST', 'http://casual-coder.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username, password:password}));
-
+  submit.value = 'Logging in...';
 };
+
+ var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
